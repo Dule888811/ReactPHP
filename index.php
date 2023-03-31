8472fc6ffa4082947017f8b3d20f9d18f91f4ae3
Dule888811 <dusan.pavlovic88@yahoo.com>
2023-03-31 22:09:50 +0200

    array numbers

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ require 'vendor/autoload.php';
 Factory::create()->detect('C:\xampp\htdocs\allip.example1 (1)\example1\db.txt')->then(function (FileInterface $file) {
     return $file->getContents();
 })->then(static function (string $contents): void {
-    echo $contents;
+  $numbers = preg_replace('/[^0-9]/', ' ', $contents);
+  $numbersArray = explode(" ",$numbers);
+    $numbersArray = \array_diff($numbersArray, [""]);
+ echo  $contents; var_dump($numbers,$numbersArray);
 })->done();
 /*Factory::create()->detect('C:\xampp\htdocs\allip.example1 (1)\example1\input.txt')->then(function (FileInterface $file) {
     return $file->getContents();
