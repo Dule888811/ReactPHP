f60df1e6adb11c89971757117cfd61410b857c0c
Dule888811 <dusan.pavlovic88@yahoo.com>
2023-04-01 10:11:24 +0200

    add output.txt

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 use React\Filesystem\Factory;
 use React\Filesystem\Node\FileInterface;
+use React\Filesystem\Node\NotExistInterface;
 use React\Filesystem\Stat;
+use React\Promise\PromiseInterface;
+
 require 'vendor/autoload.php';
 
 Factory::create()->detect('C:\xampp\htdocs\allip.example1 (1)\example1\db.txt')->then(function (FileInterface $file) {
@@ -27,6 +30,19 @@ Factory::create()->detect('C:\xampp\htdocs\allip.example1 (1)\example1\input.txt
     global $numbersArray;
     $numbersArray = preg_split('/\r\n|\r|\n/',$contents);
     echo "<pre>";
-    var_dump($numbersArray);
-  //  echo $contents;
 })->done();
+
+Factory::create()->detect('C:\xampp\htdocs\allip.example1 (1)\example1\output.txt' )->then(static function (NotExistInterface $node): PromiseInterface {
+    return $node->createFile();
+})->then(static function (FileInterface $file): PromiseInterface {
+    return $file->stat();
+})->then(static function (Stat $stat): void {
+    echo $stat->path(), ': ', get_class($stat), PHP_EOL;
+    echo 'Mode: ', $stat->mode(), PHP_EOL;
+    echo 'Uid: ', $stat->uid(), PHP_EOL;
+    echo 'Gid: ', $stat->gid(), PHP_EOL;
+    echo 'Size: ', $stat->size(), PHP_EOL;
+    echo 'Atime: ', $stat->atime()->format(DATE_ISO8601), PHP_EOL;
+    echo 'Mtime: ', $stat->mtime()->format(DATE_ISO8601), PHP_EOL;
+    echo 'Ctime: ', $stat->ctime()->format(DATE_ISO8601), PHP_EOL;
+})->done();  
