86c5a5730e57c35cf1b05c5c1cd69c8db847651d
Dule888811 <dusan.pavlovic88@yahoo.com>
2023-04-01 15:20:54 +0200

    output.txt finish app

--- index.php
+++ index.php
@@ -29,10 +29,9 @@ Factory::create()->detect('C:\xampp\htdocs\allip.example1 (1)\example1\input.txt
 })->then(static function (string $contents): void {
     global $numbersArray;
     $numbersArray = preg_split('/\r\n|\r|\n/',$contents);
-    echo "<pre>";
 })->done();
 
-Factory::create()->detect('C:\xampp\htdocs\allip.example1 (1)\example1\output.txt' )->then(static function (NotExistInterface $node): PromiseInterface {
+/*Factory::create()->detect('C:\xampp\htdocs\allip.example1 (1)\example1\output.txt' )->then(static function (NotExistInterface $node): PromiseInterface {
     return $node->createFile();
 })->then(static function (FileInterface $file): PromiseInterface {
     return $file->stat();
@@ -45,4 +44,41 @@ Factory::create()->detect('C:\xampp\htdocs\allip.example1 (1)\example1\output.tx
     echo 'Atime: ', $stat->atime()->format(DATE_ISO8601), PHP_EOL;
     echo 'Mtime: ', $stat->mtime()->format(DATE_ISO8601), PHP_EOL;
     echo 'Ctime: ', $stat->ctime()->format(DATE_ISO8601), PHP_EOL;
-})->done();  
+})->done();  */
+global $numCountry;
+
+foreach($numbersArray as $number)
+{
+    if(strlen($number) == 10)
+    {
+        $numCountry[] = substr($number,0,3);
+    }
+    if(strlen($number) == 11)
+    {
+        $numCountry[] = substr($number,0,4);
+    }
+    if(strlen($number) == 9)
+    {
+        $numCountry[] = substr($number,0,2);
+    }
+    if(strlen($number) == 8)
+    {
+        $numCountry[] = substr($number,0,1);
+    }
+}
+
+foreach ($numCountry as $nCountry)
+{
+    global $string;
+    $key = array_search($nCountry,$numArr);
+    $string .= $countriesArray[$key];
+    $string .= "\n";
+}
+
+
+Factory::create()->detect('C:\xampp\htdocs\allip.example1 (1)\example1\output.txt')->then(static function (FileInterface $file) use ($string, $numbersArray,$countriesArray,$numArr) {
+    return $file->putContents($string);
+})->then(static function ($result) : void {
+    var_export([$result]);
+})->done();
+
